56368b7ede3d8f18044a5c3eeed00e17122ace0e
Rian CD <20231ireads0009@ifba.edu.br>
2025-05-11 20:05:19 -0300

    initializing project

---
+++ librarysystem/src/test/java/br/com/ifba/library/system/LibrarysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.library.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
