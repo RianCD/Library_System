7500db0a09076d4e5818d7d1b91d58e9d7c6f5b4
Rian CD <20231ireads0009@ifba.edu.br>
2025-05-17 23:11:05 -0300

    Added: Controller classes

--- librarysystem/src/main/java/br/com/ifba/library/system/book/service/BookService.java
+++ librarysystem/src/main/java/br/com/ifba/library/system/book/service/BookService.java
@@ -3,9 +3,12 @@ package br.com.ifba.library.system.book.service;
 import br.com.ifba.library.system.book.entity.Book;
 import br.com.ifba.library.system.book.repository.BookIRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Slf4j
+@Service
 @RequiredArgsConstructor
 public class BookService implements BookIService{
     private final BookIRepository bookIRepository;
@@ -13,7 +16,14 @@ public class BookService implements BookIService{
 
     @Override
     public Book saveBook(Book book) {
-        return null;
+
+        try {
+            log.info("Book Saved!");
+            return bookIRepository.save(book);
+        } catch (Exception e) {
+            log.error("Book not saved: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
---
+++ librarysystem/src/main/java/br/com/ifba/library/system/loan/controller/LoanController.java
@@ -0,0 +1,9 @@
+package br.com.ifba.library.system.loan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+}
